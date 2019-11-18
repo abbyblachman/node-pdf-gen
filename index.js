183c17d81b27f49182a09c489f3f904fcf8c80a9
Abby Blachman <ablachman@Abbys-MacBook-Pro.local>
2019-11-18 17:20:47 -0600

    commenting code out

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+// installing npm packages and defining global variables 
 const axios = require("axios");
 const inquirer = require("inquirer");
 const pdf = require('html-pdf');
 var options = { format: 'Letter' };
 
 
-
+// using inquirer package to give user specific command-line prompts that will be variables used to generate output
 inquirer
   .prompt([{
     message: "Enter your GitHub username:",
@@ -14,17 +15,23 @@ inquirer
     type: "input",
     message: "What is your favorite color?",
     name: "color"
-  }])
+  }]) 
+  // using .then statement 
   .then(function(response) {
     const queryUrlOne = `https://api.github.com/users/${response.username}/repos`
+    // using axios call to connect with GitHub API 
+    // returning number of combined stars for users GitHub repos 
     axios.get(queryUrlOne).then(function(response) {
       return response.data.reduce((acc, curr) => {
         acc += curr.stargazers_count;
         return acc;
       }, 0);
+      // passing `acc` so it can be used in the next .then block 
     }).then(acc => {
+    // using axios call to connect with GitHub api. This links to a JSON file that displays info about user's profile. Username variable is from inquirer prompt
     const queryUrl = `https://api.github.com/users/${response.username}`;
     axios.get(queryUrl).then(async function(res) {
+      // defining variables from axios response 
       const colorName = response.color;
       const profileImg = res.data.avatar_url;
       const userName = res.data.login;
@@ -35,10 +42,7 @@ inquirer
       const followers = res.data.followers;
       const following = res.data.following;
       const userLocation = res.data.location;
-      //const repoNames = res.data.map(function(repo) {
-        //return repo.name;
-      //})
-      //const repoNamesStr = repoNames.join("\n");
+      //passing variables into HTML string 
       return htmlStr = `
       <!DOCTYPE html>
 <html lang="en">
@@ -96,7 +100,7 @@ inquirer
 </html>
 `;
     })
-
+    // passing HTML string variable into next .then, outputting pdf file using `html-pdf` package 
     .then(htmlStr => {
  
     pdf.create(htmlStr, options).toFile('./file.pdf', function(err, res) {
